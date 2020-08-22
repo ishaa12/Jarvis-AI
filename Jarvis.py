811006465b2e24bf1167c453f54612b7c4cc8c4a
Isha Dayal Sharma <55995615+ishaa12@users.noreply.github.com>
2020-08-23 02:03:50 +0530

    Add files via upload

---
+++ Jarvis.py
@@ -0,0 +1,71 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[0].id)
+
+engine.setProperty('voice',voices[1].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    hour= int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+    speak("I am Lexi . Please tell me how may i help you")
+
+
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")
+        query = r.recognize_google(audio,language='en-in')
+        print(f"User said:{query}\n")
+    except Exception as e:
+        #print(e)
+
+        print("Say that again please...")
+        return "None"
+    return query
+
+
+
+if __name__ == '__main__':
+    wishme()
+    while true:
+    #if 1:
+        query = takecommand().lower()
+
+
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia....")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to wikipedia")
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open stack overflow' in query:
+            webbrowser.open("stack overflow.com")
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Mam the time is {strTime}")
+
+    #speak("Hello Isha")
